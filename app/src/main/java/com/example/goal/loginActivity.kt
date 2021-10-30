c91674aaa2fc3e58acdc23989425a599578f90ec
BNOVE <bruno.morais05@hotmail.com>
2021-10-30 18:49:42 -0300

    commitInicial

---
+++ app/src/main/java/com/example/goal/loginActivity.kt
@@ -0,0 +1,10 @@
+package com.example.goal
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.login.*
+class loginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+     }
